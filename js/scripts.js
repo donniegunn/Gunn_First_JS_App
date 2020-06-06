4f2fa37b71cbb7ec1fda4175d1fa0a2622a5cde2
donniegunn <64431171+donniegunn@users.noreply.github.com>
2020-06-05 21:23:45 -0600

    Added "FOR" loop
    Added the For loop and tested functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
+
 var pokemonList = [
-  { name: 'Lugia', height: 5.2, types: ['Psychic, Flying'] },
-  { name: 'Machamp', height: 1.6, types: ['Fighting'] },
-  { name: 'Nuzleaf', height: 1, types: ['Dark, Grass'] }
- ]
+  {
+   name : 'Lugia',
+   height : 5.2,
+   types : ['Psychic', 'Flying'],
+  },
+  {
+   name : 'Machamp',
+   height : 1.06,
+   types : ['Grass'],
+  },
+  {
+   name : 'Nuzleaf',
+   height : 1.0,
+   types : ['Dark', 'Grass'],
+  }
+];
+
+for ( let i = 0; i < pokemonList.length; i++ ) {
+	document.write ( '<u>' + '<h3>' + pokemonList[i].name + '</h3>' + '</u>' + " " + 'Height:' + " " + pokemonList[i].height + " " )
+if (pokemonList[i].height >= 5.0) {
+	document.write ( '<i>' + '<b>' +  'Wow that\'s big!' + '</i>' + '</b>' )
+  }
+}
