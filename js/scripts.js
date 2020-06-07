0fac50b160c6a36c9003a34d950df36b9efe9f0c
donniegunn <64431171+donniegunn@users.noreply.github.com>
2020-06-07 14:58:25 -0600

    Added loop fumction and IIFE
    Made necessary adjustments to code to allow for IIFE and loop function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
+
 var pokemonRepository = (function () {
-var pokemonList = [
-  {
+  var pokemonList = [
+   {
    name : 'Lugia',
    height : 5.2,
    types : ['Psychic', 'Flying'],
@@ -14,33 +15,28 @@ var pokemonList = [
    name : 'Nuzleaf',
    height : 1.0,
    types : ['Dark', 'Grass'],
-  }
-];
-
-  function getAll() {
-    return pokemonList;
-  }
-
-function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
+  }];
 
   return {
-    add: add,
-    getAll: getAll
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
   };
 })();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Charizard' });
-console.log(pokemonRepository.getAll()); 
+pokemonRepository.add({ name: 'Charmander' });
+Object.keys(pokemonRepository.getAll()).forEach(function(attributes) {
+console.log(pokemonRepository.getAll()[attributes]);
+});
 
-
-function loopFunction() {
-  	document.write ( '<u>' + '<h3>' + pokemonRepository.getAll.name + '</h3>' + '</u>' + " " + 'Height:' + " " + pokemonRepository.getAll.height + " " );
-  	if (pokemonRepository.getAll.height >= 5.0) {
+function loopFunction(pokemon) {
+  	document.write ( '<u>' + '<h3>' + pokemon.name + '</h3>' + '</u>' + " " + 'Height:' + " " + pokemon.height + " ");
+  	if (pokemon.height >= 5.0) {
 	document.write ( '<i>' + '<b>' +  'Wow that\'s big!' + '</i>' + '</b>' );
 	}
 }
-
-pokemonRepository.getAll.forEach(loopFunction);
+pokemonRepository.getAll().forEach(loopFunction);
